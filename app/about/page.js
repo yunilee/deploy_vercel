6b01fcce2276fdd99b9354893103957a8b2a1623
Nitin Kumar <8hmv1k@gmail.com>
2024-10-29 05:08:07 +0530

    ADDING imgage

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,10 @@
+import "./about.css"
 export default function About(){
     return(
         <div>
-            <h1>This is about Nitin kumar</h1>
-        </div>
+            
+            <img className="img1" src="https://media.istockphoto.com/id/1137365972/photo/close-up-of-steaming-cup-of-coffee-or-tea-on-vintage-table-early-morning-breakfast-on-rustic.jpg?s=612x612&w=0&k=20&c=-XbehJve0dz0daS_jVi2VLmlVMGrwPIQRdaFWChKFsM=" alt="this is image"/>
+            <h1 className="h1">This is about Nitin kumar</h1>
+       </div>
     )
 }
