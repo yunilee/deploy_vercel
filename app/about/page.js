fd846cbf1e3f49c0ef0f159ea83e7e903a5bf5a8
Nitin Kumar <8hmv1k@gmail.com>
2024-10-29 04:00:10 +0530

    adding button

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function About(){
+    return(
+        <div>
+            <h1>This is about Nitin kumar</h1>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "./component/button";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Button/>
       
       <main className={styles.main}>
         {/* <Image
